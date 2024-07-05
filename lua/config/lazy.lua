14d58260475b1e4d4285db2a1023fc451e85d56c
phucnv282 <phucnv282@gmail.com>
2024-07-05 21:18:43 +0700

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     -- import/override with your plugins
     { import = "plugins" },
   },
